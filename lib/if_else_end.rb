c7605392a0d7c26acd71f10b07efa289a64a3f91
dbpatnodee <dbpatnode@hotmail.com>
2020-06-20 15:29:18 -0700

    Done.

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,8 +1,21 @@
+#solution 1
+
+current_time = Time.now
+current_time = current_time.to_i
+
+if current_time % 2 == 0
+  puts "Even!"
+else puts 
+  puts "Odd"
+end
+
+# OR solution 2
+
 current_time = Time.now
 current_time = current_time.to_i
 
 if current_time.even?
   puts "Even!"
-else
+else 
   puts "Odd!"
 end
